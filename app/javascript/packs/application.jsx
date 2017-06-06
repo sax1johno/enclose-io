ce28778af7cc008f00d8ba7a2a44083e13b56313
Minqi Pan <pmq2001@gmail.com>
2017-06-06 12:31:19 +0800

    use antd

--- app/javascript/packs/application.jsx
+++ app/javascript/packs/application.jsx
@@ -7,4 +7,15 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-console.log('Hello World from Webpacker')
+import 'antd/dist/antd.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+import { DatePicker } from 'antd';
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    <DatePicker />,
+    document.body.appendChild(document.createElement('div')),
+  );
+});
