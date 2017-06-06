f6960dd8b512689b9fc8b2709f039ef52d627655
Minqi Pan <pmq2001@gmail.com>
2017-06-06 11:38:01 +0800

    add Executable

---
+++ app/models/executable.rb
@@ -0,0 +1,2 @@
+class Executable < ApplicationRecord
+end
---
+++ db/migrate/20170606033553_create_executables.rb
@@ -0,0 +1,8 @@
+class CreateExecutables < ActiveRecord::Migration[5.1]
+  def change
+    create_table :executables do |t|
+
+      t.timestamps
+    end
+  end
+end
